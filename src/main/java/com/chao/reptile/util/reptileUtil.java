950bfcf23070469e26be75ddb73bbd2736a7244f
chao <261668497@qq.com>
2019-03-14 01:00:38 +0800

    third

--- src/main/java/com/chao/reptile/util/reptileUtil.java
+++ src/main/java/com/chao/reptile/util/reptileUtil.java
@@ -1,6 +1,35 @@
 package com.chao.reptile.util;
 
+
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class reptileUtil
 {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String pageUrl="https://www.661zh.com/pic/html28/{pageNumber}.html";
+
+        getImage(pageUrl, 14520,500);
+
+    }
+
+
+    public static void getImage(String pageUrl, int beginPage, int totalSize)
+    {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+
+        for(int i = 0; i < totalSize; i++)
+        {
+            String currentPage = beginPage-i +"";
+
+            String currentPageUrl = pageUrl.replace("{pageNumber}", currentPage);
+
+            GetImageTask getImageTask = new GetImageTask(currentPageUrl);
+
+            executorService.execute(getImageTask);
+        }
+
+    }
 
 }
